0ad2a79478a30ad993262bf9ec3f70c2f01d6a86
Kyle Zwarich <azwarich@ualberta.ca>
2024-02-26 20:40:38 -0700

    Dealing with opening a camera preview frame

--- SeQR/app/build.gradle.kts
+++ SeQR/app/build.gradle.kts
@@ -26,10 +26,35 @@ android {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
+
+    //add some view convenience features for the camera
+    buildFeatures {
+        viewBinding = true
+    }
 }
 
 dependencies {
+    //add dependencies for camera access
+    var camerax_version = "1.3.1"
+    implementation("androidx.camera:camera-core:${camerax_version}")
+    implementation("androidx.camera:camera-camera2:${camerax_version}")
+    implementation("androidx.camera:camera-lifecycle:${camerax_version}")
+    implementation("androidx.camera:camera-video:${camerax_version}")
+    implementation("androidx.camera:camera-view:${camerax_version}")
+    implementation("androidx.camera:camera-extensions:${camerax_version}")
 
+    //add dependencies for navigation
+    val nav_version = "2.7.7"
+    implementation("androidx.navigation:navigation-fragment:$nav_version")
+    implementation("androidx.navigation:navigation-ui:$nav_version")
+    implementation("androidx.navigation:navigation-dynamic-features-fragment:$nav_version")
+    androidTestImplementation("androidx.navigation:navigation-testing:$nav_version")
+
+    //add dependencies for permissions window
+    implementation("androidx.activity:activity:1.8.2")
+    implementation("androidx.fragment:fragment:1.6.2")
+
+    //base dependencies (from default new project)
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
