cbadf38b098d7b62e6de44fae06e1cc3635d1de4
Bipbos <45457731+Xunzer@users.noreply.github.com>
2024-03-05 21:27:57 -0700

    Profile edit fragment fixed
    111

--- SeQR/app/src/main/java/com/example/seqr/CEventImageFragment.java
+++ SeQR/app/src/main/java/com/example/seqr/CEventImageFragment.java
@@ -1,18 +1,18 @@
 package com.example.seqr;
 
 import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import androidx.fragment.app.Fragment;
-
-public class EditProfileFragment extends Fragment {
+public class CEventImageFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_edit_profile, container, false);
+        return inflater.inflate(R.layout.fragment_c_event_image, container, false);
     }
 }
-
