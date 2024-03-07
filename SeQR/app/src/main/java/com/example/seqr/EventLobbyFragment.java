8b86e05c1f35bd978298342f513bafd033964c89
Bipbos <45457731+Xunzer@users.noreply.github.com>
2024-03-07 15:21:18 -0700

    Updated event lobby and attendee frag

--- SeQR/app/src/main/java/com/example/seqr/EventLobbyFragment.java
+++ SeQR/app/src/main/java/com/example/seqr/EventLobbyFragment.java
@@ -3,17 +3,51 @@ package com.example.seqr;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.seqr.adapters.EventAdapter;
+import com.example.seqr.controllers.EventController;
+import com.example.seqr.models.Event;
+import com.google.firebase.firestore.DocumentSnapshot;
+
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class EventLobbyFragment extends Fragment {
+    private RecyclerView recyclerView;
+    private EventAdapter eventAdapter;
+    private List<Event> eventsList;
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_event_lobby, container, false);
+        View view = inflater.inflate(R.layout.fragment_event_lobby, container, false);
+
+        recyclerView = view.findViewById(R.id.eventLobbyRecyclerview);
+        eventsList = new ArrayList<>();
+        eventAdapter = new EventAdapter(eventsList);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        recyclerView.setAdapter(eventAdapter);
+
+        // get the events to event lobby
+        EventController eventController = new EventController();
+        eventController.getAllEvents(task -> {
+            if (task.isSuccessful()) {
+                for (DocumentSnapshot document : task.getResult()) {
+                    Event event = document.toObject(Event.class);
+                    eventsList.add(event);
+                }
+                eventAdapter.notifyDataSetChanged();
+            } else {
+                Log.d("DEBUG", "there was some error in event retrieval to event lobby", task.getException());
+            }
+        });
+        return view;
     }
 }
