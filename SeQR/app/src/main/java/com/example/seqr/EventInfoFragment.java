a3d538d8f6b6dbfa6d323ff48b75b5ec373fdebd
Ahmed Abdul-Rafay <arafay0110@gmail.com>
2024-03-08 00:43:00 -0700

    added another xml

--- SeQR/app/src/main/java/com/example/seqr/EventInfoFragment.java
+++ SeQR/app/src/main/java/com/example/seqr/EventInfoFragment.java
@@ -7,6 +7,8 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 
 public class EventInfoFragment extends Fragment {
@@ -15,6 +17,17 @@ public class EventInfoFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_event_info, container, false);
+        View view = inflater.inflate(R.layout.fragment_event_info, container, false);
+
+        TextView eventNameText = view.findViewById(R.id.eventInfoNameText);
+        TextView eventOrganizer = view.findViewById(R.id.eventInfoOrganizer);
+        TextView eventLocation = view.findViewById(R.id.eventInfoLocation);
+        TextView eventTime = view.findViewById(R.id.eventInfoTime);
+        TextView eventCapacity = view.findViewById(R.id.eventInfoCapacity);
+        ImageView eventPhoto = view.findViewById(R.id.eventPhotoPreview);
+        TextView eventDescription = view.findViewById(R.id.eventInfoDescription);
+
+        return view;
+
     }
 }
