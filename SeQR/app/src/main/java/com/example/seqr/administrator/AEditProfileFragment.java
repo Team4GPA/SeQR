fd0ba8889643cbebd53c28741e06d3557ed5dd48
chaseufc <103541454+chaseufc@users.noreply.github.com>
2024-04-07 15:23:53 -0600

    Now can edit and remove profile picture when editing profile

--- SeQR/app/src/main/java/com/example/seqr/administrator/AEditProfileFragment.java
+++ SeQR/app/src/main/java/com/example/seqr/administrator/AEditProfileFragment.java
@@ -1,5 +1,10 @@
 package com.example.seqr.administrator;
 
+import static android.app.Activity.RESULT_OK;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
@@ -10,14 +15,21 @@ import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.example.seqr.MainActivity;
 import com.example.seqr.controllers.ProfileController;
 import com.example.seqr.database.Database;
+import com.example.seqr.helpers.ImageUploader;
+import com.example.seqr.models.ID;
 import com.example.seqr.profile.DeleteProfileFragment;
 import com.example.seqr.R;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.Firebase;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -41,6 +53,9 @@ public class AEditProfileFragment extends Fragment{
     private EditText homePageEditText;
     private ImageView profilePicView;
     private CheckBox isAdminCheckBox;
+    private static final int PICK_IMAGE_REQUEST = 1;
+
+    private Uri imageUri;
 
     public AEditProfileFragment() {
         storage = Database.getStorage();
@@ -65,6 +80,8 @@ public class AEditProfileFragment extends Fragment{
         Button deleteProfileButton = view.findViewById(R.id.admin_delete_profile_button);
         Button backButton = view.findViewById(R.id.admin_edit_profile_back_button);
         Button removeProfilePicButton = view.findViewById(R.id.admin_remove_profile_picture_button);
+        Button submitChangesButton = view.findViewById(R.id.admin_edit_personal_info_button);
+        Button editProfilePicButton = view.findViewById(R.id.admin_edit_profile_picture_button);
 
         ProfileController profileController = new ProfileController();
         // Unpack Bundle
@@ -78,19 +95,26 @@ public class AEditProfileFragment extends Fragment{
         Boolean isAdmin = bundle.getBoolean("isAdmin",false);
 
         // Get specific fields to fill in
-        EditText nameEditText = view.findViewById(R.id.admin_profile_name);
-        EditText phoneNumberEditText = view.findViewById(R.id.admin_profile_number);
-        EditText emailEditText = view.findViewById(R.id.admin_profile_email);
-        EditText homePageEditText = view.findViewById(R.id.admin_profile_homepage);
-        ImageView profilePicView = view.findViewById(R.id.admin_edit_profile_picture);
-        CheckBox isAdminCheckBox = view.findViewById(R.id.admin_edit_profile_admin_checkbox);
+        nameEditText = view.findViewById(R.id.admin_profile_name);
+        phoneNumberEditText = view.findViewById(R.id.admin_profile_number);
+        emailEditText = view.findViewById(R.id.admin_profile_email);
+        homePageEditText = view.findViewById(R.id.admin_profile_homepage);
+        profilePicView = view.findViewById(R.id.admin_edit_profile_picture);
+        isAdminCheckBox = view.findViewById(R.id.admin_edit_profile_admin_checkbox);
 
         // Set the fields to the values of the profile
         nameEditText.setText(username);
         emailEditText.setText(email);
-        phoneNumberEditText.setText(phoneNumber);
+        if (phoneNumber.equals("null")){
+            phoneNumberEditText.setHint("Enter Phone Number");
+        } else {
+            phoneNumberEditText.setText(phoneNumber);
+        }
+
         homePageEditText.setText(homePage);
         isAdminCheckBox.setChecked(isAdmin);
+
+
 
         String path = Uri.encode("ProfilePictures/"+id+".jpg");
         String imageUrl = "https://firebasestorage.googleapis.com/v0/b/seqr-177ac.appspot.com/o/" + path + "?alt=media";
@@ -105,10 +129,73 @@ public class AEditProfileFragment extends Fragment{
             }
         });
 
+        editProfilePicButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openFileChooser();
+            }
+        });
         removeProfilePicButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 profileController.deleteProfilePicture(id);
+
+            }
+        });
+
+        submitChangesButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                boolean isLegal = true;
+                // Retrieve text entered in EditText views
+                String username = nameEditText.getText().toString();
+                String phoneNumber = phoneNumberEditText.getText().toString();
+                String email = emailEditText.getText().toString();
+                String homepage = homePageEditText.getText().toString();
+
+                // Get the user's UUID
+                String uuid = ID.getProfileId(getContext());
+
+                if (username.isEmpty()){
+                    nameEditText.setError("Please enter a username!");
+                    isLegal = false;
+                } else if (username.length() > 30) {
+                    nameEditText.setError("Username must be less than 30 characters!");
+                    isLegal = false;
+                }
+
+                if (phoneNumber.isEmpty()){
+                    phoneNumberEditText.setError("Please enter a phone number");
+                    isLegal = false;
+                } else if (phoneNumber.length() > 30){
+                    phoneNumberEditText.setError("Phone Number is too long!");
+                    isLegal = false;
+                }
+
+                if (email.isEmpty()){
+                    emailEditText.setError("Please enter an email");
+                    isLegal = false;
+                } else if (phoneNumber.length() > 30){
+                    emailEditText.setError("Email is too long!");
+                }
+
+                if (homePage.isEmpty()){
+                    homePageEditText.setError("Please enter a homepage");
+                    isLegal = false;
+                } else if (homepage.length() > 30){
+                    homePageEditText.setError("Homepage is too long!");
+                    isLegal = false;
+                }
+
+                // Update profile information in Firestore
+                if (isLegal){
+                    if (uuid != null) {
+                        ProfileController profileController = new ProfileController();
+                        profileController.updateProfile(uuid, username, phoneNumber, email, homepage);
+                    }
+                    getParentFragmentManager().popBackStack();
+                }
 
             }
         });
@@ -135,4 +222,61 @@ public class AEditProfileFragment extends Fragment{
         dialogFragment.show(getChildFragmentManager(), "DeleteProfileFragment");
     }
 
+
+    /**
+     * Opens a file chooser for selecting an image to upload as your profile picture.
+     */
+    private void openFileChooser() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(intent, PICK_IMAGE_REQUEST);
+    }
+
+    /**
+     * Called when an activity you launched exits, giving you the requestCode you started it with,
+     * the resultCode it returned, and any additional data (new profile picture) from it.
+     */
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && data != null && data.getData() != null) {
+            imageUri = data.getData();
+            profilePicView.setImageURI(imageUri);
+            Picasso.get().load(imageUri).into(profilePicView);
+            setProfilePicture(imageUri);
+            ((MainActivity) getActivity()).setFirstTime(false);
+        }
+    }
+
+    // Method to generate a new profile picture
+    public void setProfilePicture(Uri imageUri) {
+
+        System.out.println("SUCCESSFUL ACCESS");
+        // Update profile picture URL in Firestore
+        String uuid = ID.getProfileId(getContext());
+        if (uuid != null) {
+            ImageUploader iuploader = new ImageUploader("ProfilePictures");
+            iuploader.upload(imageUri, uuid);
+            // Update stored URI in SharedPreferences
+            SharedPreferences sharedPreferences = requireActivity().getSharedPreferences("Profile", Context.MODE_PRIVATE);
+            SharedPreferences.Editor editor = sharedPreferences.edit();
+            editor.putString("profile_picture_uri", imageUri.toString());
+            editor.apply();
+            // Update profile picture URL in Firestore
+            ProfileController profileController = new ProfileController();
+            profileController.updatePFP(uuid, imageUri.toString(), new OnSuccessListener<Void>() {
+                @Override
+                public void onSuccess(Void aVoid) {
+                    // Notify MainActivity about the profile picture update
+                    ((MainActivity) getActivity()).updateProfilePicture(imageUri);
+                }
+            }, new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    Log.d("DEBUG", "Error updating profile picture", e);
+                }
+            });
+        }
+    }
 }
