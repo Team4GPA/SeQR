26795f12f54cc7554ccff9433616c0cc449ccf21
Ahmed Abdul-Rafay <arafay0110@gmail.com>
2024-04-08 12:00:04 -0600

    added announcement controller test

--- SeQR/app/build.gradle.kts
+++ SeQR/app/build.gradle.kts
@@ -72,6 +72,8 @@ dependencies {
     implementation("com.google.firebase:firebase-messaging")
     implementation("com.google.firebase:firebase-analytics")
     testImplementation("junit:junit:4.13.2")
+    testImplementation("org.mockito:mockito-core:4.5.1")
+    testImplementation("org.robolectric:robolectric:4.12.1")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     androidTestImplementation("androidx.test:core:1.5.0")
--- SeQR/app/src/test/java/com/example/seqr/AnnouncementControllerTest.java
+++ SeQR/app/src/test/java/com/example/seqr/AnnouncementControllerTest.java
@@ -1,6 +1,83 @@
 package com.example.seqr;
 
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
+import com.example.seqr.controllers.AnnouncementController;
+import com.example.seqr.models.Announcement;
+import com.google.android.gms.tasks.Task;
+import com.google.android.gms.tasks.Tasks;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
+
+
+@RunWith(RobolectricTestRunner.class) //Run the tests using Robolectric
+@Config(manifest = Config.NONE) // dont need the manifest files for this test
 public class AnnouncementControllerTest {
-    //
+    //creates a mock object for firestore
+    @Mock
+    private FirebaseFirestore mockFirestore;
+    //creates a mock collection reference
+    @Mock
+    private CollectionReference mockAnnouncementCollection;
+    //makes a mock document reference
+    @Mock
+    private DocumentReference mockAnnouncementDoc;
+
+    //announcement controller instance which we are testing
+    private AnnouncementController announcementController;
+
+
+    //this should be run before every test
+
+    @Before
+    public void setUp(){
+        MockitoAnnotations.openMocks(this); // initializes mockFireStore, mockAnnouncementCollection, mockAnnouncementDoc
+        //This is basically mocking getting a collection Announcements from firestore
+        //when we call mockFirestore.collection("Announcements") we want mockAnnouncementCollection returned
+        when(mockFirestore.collection("Announcements")).thenReturn(mockAnnouncementCollection);
+
+        //mocks getting a document from firestore, basiclly saying when we call mockAnnouncementCollection.document(anyString())
+        //return a mockDoc
+        when(mockAnnouncementCollection.document(anyString())).thenReturn(mockAnnouncementDoc);
+        //create an announcementController with the mocked firestore
+        announcementController = new AnnouncementController(mockFirestore);
+    }
+
+    @Test
+    public void testAddAnnouncement(){
+        Announcement announcement = new Announcement(); //a new announcement object
+        announcement.setAnnouncementID("Testing announcementID"); //set its id
+        Task<Void> mockTask = Tasks.forResult(null); //makes a mock task object that has no result this is for methods that just have no return value just success or failure
+
+        //when we call mockAnnouncementDoc.set(announcement) it should return a task
+        when(mockAnnouncementDoc.set(announcement)).thenReturn(mockTask);
+        
+        announcementController.addAnnouncement(announcement);
+
+        verify(mockAnnouncementCollection).document("Testing announcementID");
+        verify(mockAnnouncementDoc).set(announcement);
+    }
+
+    @Test
+    public void testRemoveAnnouncementByID(){
+        Task<Void> mockTask = Tasks.forResult(null);
+        when(mockAnnouncementDoc.delete()).thenReturn(mockTask);
+
+        announcementController.removeAnnouncementByID("Testing announcementID");
+        verify(mockAnnouncementCollection).document("Testing announcementID");
+        verify(mockAnnouncementDoc).delete();
+
+    }
 }
