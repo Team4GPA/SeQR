3b9d43151b674c83ee552d99d7e1c72aa54dffbd
Ahmed Abdul-Rafay <arafay0110@gmail.com>
2024-04-07 22:44:04 -0600

    moved the deleted profile check into attendee fragment

--- SeQR/app/src/main/java/com/example/seqr/MainActivity.java
+++ SeQR/app/src/main/java/com/example/seqr/MainActivity.java
@@ -82,27 +82,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
         String uuid = ID.getProfileId(getBaseContext());
-        System.out.println(uuid);
-        if (uuid != null){
-            ProfileController profileController = new ProfileController();
-            profileController.getProfileUsernameByDeviceId(uuid, new OnCompleteListener<DocumentSnapshot>() {
-                @Override
-                public void onComplete(@NonNull Task<DocumentSnapshot> task) {
-                    if (task.isSuccessful()){
-                        DocumentSnapshot profileDoc = task.getResult();
-                        if (!profileDoc.exists() || profileDoc == null){
-                            ID.removeProfileID(getBaseContext());
-                        }
-                    }else {
-                        Log.d("DEBUG","User is still valid in firebase");
-                    }
-                }
-            });
-        }
-
-
-
         if (uuid == null) {
             FragmentManager fragMgr = getSupportFragmentManager();
             View mainFrag = findViewById(R.id.fragment_container);
@@ -123,9 +105,9 @@ public class MainActivity extends AppCompatActivity {
             }
 
             profileController.getProfileUsernameByDeviceId(uuid, new OnCompleteListener<DocumentSnapshot>() {
-            @Override
+                @Override
                 public void onComplete(@NonNull Task<DocumentSnapshot> task) {
-                    if(task.isSuccessful()){
+                    if (task.isSuccessful()) {
                         DocumentSnapshot documentSnapshot = task.getResult();
                         if (documentSnapshot != null && documentSnapshot.exists()) {
                             boolean geoLocationEnabled = documentSnapshot.getBoolean("geoLocation");
@@ -136,34 +118,35 @@ public class MainActivity extends AppCompatActivity {
                 }
             });
 
-        // Request to enable permissions on app startup
-        String[] permission = {Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION};
-        if (ContextCompat.checkSelfPermission(getBaseContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED){
-            requestPermissions(permission,1);
-        }
-            // Add an event listener to the checkbox
-        enableGeoLocationCheckbox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                // Update the geolocation setting in Firestore for the current user
-                String uuid = ID.getProfileId(MainActivity.this);
-                if (uuid != null) {
-                    ProfileController profileController = new ProfileController();
-                    profileController.updateGeoLocation(uuid, isChecked);
-                }
-
-                // Request to enable permissions when checkbox is marked but permissions are not enabled
-                if (isChecked && ContextCompat.checkSelfPermission(getBaseContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED){
-                    requestPermissions(permission,1);
-                }
+            // Request to enable permissions on app startup
+            String[] permission = {Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION};
+            if (ContextCompat.checkSelfPermission(getBaseContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                requestPermissions(permission, 1);
             }
-        });
+            // Add an event listener to the checkbox
+            enableGeoLocationCheckbox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+                @Override
+                public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                    // Update the geolocation setting in Firestore for the current user
+                    String uuid = ID.getProfileId(MainActivity.this);
+                    if (uuid != null) {
+                        ProfileController profileController = new ProfileController();
+                        profileController.updateGeoLocation(uuid, isChecked);
+                    }
+
+                    // Request to enable permissions when checkbox is marked but permissions are not enabled
+                    if (isChecked && ContextCompat.checkSelfPermission(getBaseContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                        requestPermissions(permission, 1);
+                    }
+                }
+            });
             Button adminButton = findViewById(R.id.admin_button);
             profileImageView = findViewById(R.id.profile_picture);
             String path = Uri.encode("ProfilePictures/" + uuid + ".jpg");
             String imageUrl = "https://firebasestorage.googleapis.com/v0/b/seqr-177ac.appspot.com/o/" + path + "?alt=media";
             Picasso.get().invalidate(imageUrl);
-            Picasso.get().load(imageUrl).error(R.drawable.profile_picture_drawer_navigation_icon).into(profileImageView);}
+            Picasso.get().load(imageUrl).error(R.drawable.profile_picture_drawer_navigation_icon).into(profileImageView);
+
 
             //setup the main fragment view stuff
             FragmentManager fragMgr = getSupportFragmentManager();
@@ -304,8 +287,7 @@ public class MainActivity extends AppCompatActivity {
                             .replace(R.id.fragment_container, announcementDetailFragment)
                             .addToBackStack(null) // Add the fragment transaction to the back stack
                             .commit();
-                }
-                else {
+                } else {
                     Log.d("notfi", "It is a milestone.");
                     // milestone code
                     Bundle bundle = new Bundle();
@@ -320,7 +302,10 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         }
+
+
     }
+
 //
 //    //==============================================================================================
 //    //End of onCreate() override
